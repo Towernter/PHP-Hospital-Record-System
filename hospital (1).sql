36f9414a7987def50a205e37375c4f87c7154386
towernter <32792087+Towernter@users.noreply.github.com>
2019-12-04 07:30:06 +0200

    Update hospital (1).sql

--- hospital (1).sql
+++ hospital (1).sql
@@ -43,11 +43,7 @@ CREATE TABLE `auth_user` (
 -- Dumping data for table `auth_user`
 --
 
-INSERT INTO `auth_user` (`id`, `password`, `first_name`, `last_name`, `gender`, `email`, `is_staff`, `hospital`) VALUES
-(10424296, '$2y$10$/qB8n9QnRLLCJIH0oysbh.xNFuT2Cdvwo30/c7bClKR.pcj8d/AFS', 'Carlington1', 'Juruvenge', '', 'cjuruvenge@gmail.com', 0, 'Harare Hospital'),
-(10650878, '$2y$10$UHlG.4TynjfyikmGIBuIm./plQK37B4PySLh7jyYKKrwp7PzCSsre', 'John', 'Doe', 'Male', 'jdoe@gmail.com', 1, 'Parirenyatwa'),
-(10891707, '$2y$10$f/w3PHn6GbYg9Uw8siSi7.NEpaTCGOAxLKRDdMKFPFXV2FDUUqpfm', 'Aaron', 'Mayendesa', '', 'amayendesa@gmail.com', 0, 'Chitungwiza Hospital'),
-(10972892, '$2y$10$eSIec0OU6GZD2w9yEUYE9euckcmTpOcXF3NU3AzXQZb5EcvFj1upK', 'Millicent', 'Kabade', '', 'kabade@gmail.com', 0, 'Karanda');
+
 
 -- --------------------------------------------------------
 
@@ -67,9 +63,7 @@ CREATE TABLE `med_history` (
 -- Dumping data for table `med_history`
 --
 
-INSERT INTO `med_history` (`id`, `patient_id`, `description`, `doctor`, `added_date`) VALUES
-(10164527, 10687770, 'Flue', 'Mayendesa', '2019-11-04'),
-(10344666, 10211705, 'headeahe', 'Zunza', '2019-11-04');
+
 
 -- --------------------------------------------------------
 
@@ -98,10 +92,6 @@ CREATE TABLE `patient` (
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`id`, `id_no`, `fname`, `lname`, `email`, `dob`, `blood_type`, `address`, `hospital`, `date_added`, `nok_fname`, `nok_lname`, `nok_address`, `nok_email`) VALUES
-(10211705, '63-127E75', 'Jane ', 'Doe', 'Jndoe@gmail.com', '2018-01-09', 'AB', '		                 		3rd street\r\n		                 	', 'Karanda', '2019-11-04', 'Tom', 'Doe', '		                 		4th street\r\n		                 	', 'tdoe@gmail.com'),
-(10687770, '63-128C80', 'John', 'Doe', 'jdoe@gmail.com', '2018-10-03', 'A', '		                 		1st street\r\n		                 	', 'Harare Hospital', '2019-11-04', 'Tom', 'Doe', '		                 		2nd street\r\n		                 	', 'tdoe@gmail.com'),
-(10825778, '63-458C90', 'Tom', 'Doe', 'tdoe@gmail.com', '2019-11-01', 'A', '		                 		2nd street\r\n		                 	', 'Parirenyatwa', '2019-11-04', 'john', 'Doe', '		                 		1st street\r\n		                 	', 'jdoe@gmail.com');
 
 --
 -- Indexes for dumped tables
